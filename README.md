eb6390a44b354c773a4c3d33892faad22ceeb312
ROGERDJQ <roger_davis@sina.com>
2020-03-05 16:35:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,93 @@
+# 基于fastNLP的Aspect-Level Sentiment Analysis实践 
+
+
+## 介绍
+
+代码为Aspect-Level Sentiment Analysis Via Convolution over Dependency Tree(CDT)的[fastNLP](https://github.com/fastnlp/fastNLP)实现，基于论文[Aspect-Level Sentiment Analysis Via Convolution over Dependency Tree](https://www.aclweb.org/anthology/D19-1569/)。文章结合lstm与GCN，利用Dependency Tree的结构信息，提高了在Aspect-Term Sentiment Analysis 任务上的结果。原始实现代码在[CDT_ABSA](https://github.com/sunkaikai/CDT_ABSA)。
+
+## Requirements
+   - [fastNLP](https://github.com/fastnlp/fastNLP) 
+   - [fitlog](https://github.com/fastnlp/fitlog)
+   - PyTorch
+
+## Run
+### 数据准备
+- 接受的数据格式为json，结构如：
+```json
+{"token": ["Boot","time",…],
+"pos": ["NN","NN",…],
+"head": [2, 4,…],
+"deprel": ["compound","nsubj",…],
+"aspects": [{
+        "term": ["Boot","time"],
+        "from": 0,
+        "to": 2,
+        "polarity": "positive"}]}
+```
+- 训练需要加载GloVe vectors [glove.840B.300d.zip](https://nlp.stanford.edu/projects/glove/),加载后，将位置赋给参数`glove_dir`，具体参看代码内说明。
+### 训练
+训练模型，运行：
+```python
+python fast_gcn.py --data_dir your/data/dir/ --glove_dir your/glove/dir
+```
+更多的参数设置参看代码内说明。
+训练模型将会保存在`save_dir`。
+## 结果
+原论文共实验了四个数据集。在训练时也分别进行了实验。
+
+<table>
+   <tr>
+      <td></td>
+      <td colspan="2">Rest14</td>
+      <td colspan="2">Laptop</td>
+      <td colspan="2">Twitter</td>
+      <td colspan="2">Rest16</td>
+   </tr>
+   <tr>
+      <td>Model</td>
+      <td>Acc</td>
+      <td>F1</td>
+      <td>Acc</td>
+      <td>F1</td>
+      <td>Acc</td>
+      <td>F1</td>
+      <td>Acc</td>
+      <td>F1</td>
+   </tr>
+   <tr>
+      <td>原论文</td>
+      <td>82.30</td>
+      <td>74.02</td>
+      <td>77.19</td>
+      <td>72.99</td>
+      <td>74.66</td>
+      <td>73.66</td>
+      <td>85.58</td>
+      <td>69.93</td>
+   </tr>
+   <tr>
+      <td>本代码</td>
+      <td>82.93</td>
+      <td>74.60</td>
+      <td>77.06</td>
+      <td>73.27</td>
+      <td>74.29</td>
+      <td>72.78</td>
+      <td>85.56</td>
+      <td>66.11</td>
+   </tr>
+   <tr>
+      <td></td>
+   </tr>
+</table>
+
+## Disclaimer
+建图时tree代码为原代码中实现，处理后数据文件与参数与原repo中结构相同，见[CDT_ABSA](https://github.com/sunkaikai/CDT_ABSA)。
+
+## TODO
+
+## References
+- [Aspect-Level Sentiment Analysis Via Convolution over Dependency Tree](https://www.aclweb.org/anthology/D19-1569/), EMNLP 2019.
+- [CDT_ABSA](https://github.com/sunkaikai/CDT_ABSA)
+- [fastNLP 中文文档](https://fastnlp.readthedocs.io/zh/latest/)
+- [fitlog 中文文档](https://fitlog.readthedocs.io/zh/latest/)
---
+++ gcn.py
@@ -0,0 +1,158 @@
+import numpy as np
+import pretty_errors
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import fastNLP
+from fastNLP import (SGD, AccuracyMetric, Adam, CrossEntropyLoss, DataSet,
+                     DataSetIter, SpanFPreRecMetric, Trainer, Vocabulary)
+from fastNLP.core.callback import FitlogCallback
+from fastNLP.core.metrics import MetricBase
+from fastNLP.embeddings import StaticEmbedding
+from fastNLP.modules import LSTM,MLP
+
+class GCNClassifier(nn.Module):
+    def __init__(self, args, emb_matrix):
+        super().__init__()
+
+        in_dim = args.hidden_dim
+        self.gcn_model = GCNAbsaModel(args, emb_matrix=emb_matrix)
+        self.classifier = MLP([in_dim, args.num_class])
+
+    def forward(self, mask, chars, aspect, seq_len, head, posidx, postidx, depidx):
+        output = self.gcn_model(
+            mask, chars, aspect, seq_len, head, posidx, postidx, depidx
+        )["pred"]
+        logit = self.classifier(output)
+        return {"pred": logit}
+
+
+class GCNAbsaModel(nn.Module):
+    def __init__(self, args, emb_matrix=None):
+        super().__init__()
+        self.args = args
+
+        self.emb_matrix = emb_matrix
+        self.tok_emb = nn.Embedding(args.tok_size, args.tok_dim, padding_idx=0)
+        if emb_matrix is not None:
+            self.tok_emb = emb_matrix  
+        self.pos_emb = (
+            nn.Embedding(args.pos_size, args.pos_dim, padding_idx=0)
+            if args.pos_dim > 0
+            else None
+        )
+        self.post_emb = (
+            nn.Embedding(args.post_size, args.post_dim, padding_idx=0)
+            if args.post_dim > 0
+            else None
+        )
+        embedding = (self.tok_emb, self.pos_emb, self.post_emb)
+
+        # gcn
+        self.gcn = GCN(args, embedding, args.hidden_dim)
+
+    def forward(self, mask, chars, aspect, seq_len, head, posidx, postidx, depidx):
+        tok = chars
+        head = head
+        mask = mask.to(torch.float32)
+        seq_len = seq_len
+        maxlen = max(seq_len)
+
+        def inputs_to_tree_reps(head, words, l):
+            trees = [head_to_tree(head[i], words[i], l[i])
+                     for i in range(len(l))]
+            adj = [
+                tree_to_adj(
+                    maxlen, tree, directed=self.args.direct, self_loop=self.args.loop
+                ).reshape(1, maxlen, maxlen)
+                for tree in trees
+            ]
+            adj = np.concatenate(adj, axis=0)
+            adj = torch.from_numpy(adj)
+            return adj.cuda()
+
+        adj = inputs_to_tree_reps(head, tok, seq_len)
+        h = self.gcn(
+            adj, mask, chars, aspect, seq_len, head, posidx, postidx, depidx
+        )
+
+        asp_num = mask.sum(dim=1).unsqueeze(-1)
+        mask = mask.unsqueeze(-1).repeat(1, 1, self.args.hidden_dim)
+        output = (h["pred"] * mask).sum(dim=1) / asp_num
+
+        return {"pred": output}
+
+
+class GCN(nn.Module):
+    def __init__(self, args, embedding, hid_dim):
+        super(GCN, self).__init__()
+
+        self.args = args
+        self.layers = args.num_layers
+        self.mem_dim = hid_dim
+        self.in_dim = args.tok_dim + args.pos_dim + args.post_dim
+        self.tok_emb, self.pos_emb, self.post_emb = embedding
+        # drop out
+        self.rnn_drop = nn.Dropout(args.rnn_dropout)
+        self.in_drop = nn.Dropout(args.input_dropout)
+        self.gcn_drop = nn.Dropout(args.gcn_dropout)
+
+        # lstm
+        input_size = self.in_dim
+        self.rnn = LSTM(
+            input_size,
+            args.rnn_hidden,
+            args.rnn_layers,
+            batch_first=True,
+            dropout=args.rnn_dropout,
+            bidirectional=args.bidirect,
+        )
+        if args.bidirect:
+            self.in_dim = args.rnn_hidden * 2
+        else:
+            self.in_dim = args.rnn_hidden
+
+        # gcn layer
+        self.G = nn.ModuleList()
+        for layer in range(self.layers):
+            input_dim = [self.in_dim, self.mem_dim][layer != 0]
+            self.G.append(MLP([input_dim, self.mem_dim]))
+
+
+    def forward(self, adj, mask, chars, aspect, seq_len, head, posidx, postidx, depidx):
+        tok = chars
+        pos = posidx
+        post = postidx
+        seq_len = seq_len
+
+        word_embs = self.tok_emb(tok)
+        embs = [word_embs]
+        if self.args.pos_dim > 0:
+            embs += [self.pos_emb(pos)]
+        if self.args.post_dim > 0:
+            embs += [self.post_emb(post)]
+        embs = torch.cat(embs, dim=2)
+        embs = self.in_drop(embs)
+
+        # rnn
+        total_layers = args.rnn_layers * 2 if args.bidirect else args.rnn_layers
+        h0 = c0 = Variable(
+            torch.zeros([total_layers, tok.size()[0], self.args.rnn_hidden]),
+            requires_grad=False,
+        )
+        h0 = h0.cuda()
+        c0 = c0.cuda()
+
+        rnn_output, _ = self.rnn(embs, h0=h0, c0=c0)
+        gcn_input = self.rnn_drop(rnn_output)
+
+        # gcn
+        denom = adj.sum(2).unsqueeze(2) + 1  
+        for l in range(self.layers):
+            AX = adj.bmm(gcn_input)
+            AXW = self.G[l](AX)
+            AXW = F.relu(AXW / denom)
+            gcn_input = self.gcn_drop(AXW) if l < self.layers - 1 else AXW
+        return {"pred": gcn_input}
+
